0415347fe500053ebd4090ad31b681661e5849c6
soonhankwon <113872320+soonhankwon@users.noreply.github.com>
2022-10-13 17:02:06 +0900

    Add files via upload

---
+++ postblog2/src/test/java/com/assignment/postblog/PostblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assignment.postblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostblogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
